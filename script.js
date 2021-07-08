2c96cba32b23c1967e1a041e0f350faa02cab1f8
nithinmanoj10 <64126131+nithinmanoj10@users.noreply.github.com>
2021-07-08 13:38:40 +0530

    Side Menu Bar
    Made the side menu bar horizontal

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // SIDE-MENU
 const navBarHamburger = document.querySelector(".nav-bar__hamburger");
 const sideMenu = document.querySelector(".side-menu");
+const sideMenuList = document.querySelector(".side-menu__list");
 const overlay = document.querySelector(".overlay");
 let sideMenuActive = false;
 
 const showSideMenu = function () {
   if (sideMenuActive) {
-    sideMenu.style.transform = "translateX(100%)";
+    sideMenu.style.height = "0";
     overlay.style.display = "none";
+    sideMenuList.style.opacity = "0";
     sideMenuActive = false;
   } else if (!sideMenuActive) {
-    sideMenu.style.transform = "translateX(0)";
+    sideMenu.style.height = "70px";
     overlay.style.display = "block";
     sideMenuActive = true;
+    sideMenuList.style.opacity = "1";
   }
 };
 
