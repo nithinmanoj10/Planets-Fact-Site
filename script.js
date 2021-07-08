5533a10028ae77c34839497c415e49ee81462c8a
nithinmanoj10 <64126131+nithinmanoj10@users.noreply.github.com>
2021-07-08 20:28:45 +0530

    JavaScript added
    JS added. DOM changes when a link for the planet is clicked

--- script.js
+++ script.js
@@ -26,3 +26,55 @@ navBarHamburger.addEventListener("click", function () {
 overlay.addEventListener("click", function () {
   showSideMenu();
 });
+
+// GETTING JSON DATA
+
+let planetsData;
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    planetsData = data;
+  });
+
+// WORKING
+const planetMainLinks = document.querySelectorAll(".main-menu__list__item");
+
+// gets the planet object from data.json depensing on which link was clicked
+planetMainLinks.forEach(function (link) {
+  link.addEventListener("click", function (clickedLink) {
+    planetLink = clickedLink.target;
+    planetsData.forEach(function (planetObject) {
+      if (planetObject.name == planetLink.text) {
+        updateView(planetObject);
+      }
+    });
+  });
+});
+
+// planet is an object, gonna update the description based on the object
+const updateView = function (planet) {
+  const planetInfoHeading = document.querySelector(".planet-info__heading");
+  const planetInfoDescription = document.querySelector(
+    ".planet-info__description"
+  );
+
+  const planetRotation = document.getElementById("rotation-data");
+  const planetRevolution = document.getElementById("revolution-data");
+  const planetRadius = document.getElementById("radius-data");
+  const planetTemp = document.getElementById("temperature-data");
+  const planetPicture = document.querySelector(".planet-picture");
+
+  console.log(planetRevolution, planetRotation, planetTemp, planetRadius);
+
+  const { rotation, revolution, radius, temperature } = planet;
+  const { planet: planetPic, internal, geology } = planet.images;
+
+  planetInfoHeading.innerHTML = planet.name;
+  planetInfoDescription.innerHTML = planet.overview.content;
+  planetRotation.innerHTML = rotation;
+  planetRevolution.innerHTML = revolution;
+  planetRadius.innerHTML = radius;
+  planetTemp.innerHTML = temperature;
+  planetPicture.style.backgroundImage = `url(${planetPic})`;
+};
